45c5fee56d32f6ddc0389aff2348ab859450680c
Steve Richards <steve.richards@gmail.com>
2018-07-13 04:28:11 -0700

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  rules: {
+    'arrow-parens': 0,
+    'import/prefer-default-export': 0,
+    'no-plusplus': 0,
+    'max-len': ['error', { code: 120, ignoreTrailingComments: true }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
