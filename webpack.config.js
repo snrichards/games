f00a2986205a6723e3d4c5b9595ea3ea50d869f6
Steve Richards <steve.richards@gmail.com>
2018-07-13 03:15:14 -0700

    Create webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'cheap-module-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
