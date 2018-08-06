42658671aa840db2ebd7249ecd88b741d8e6275b
Steve Richards <steve.richards@gmail.com>
2018-08-05 22:12:45 -0700

    Add route for tower of hanoi game

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import FourOhFour from './FourOhFour';
+import TowerOfHanoi from './games/towerofhanoi';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/towerofhanoi" component={TowerOfHanoi} />
       <Route component={FourOhFour} />
     </Switch>
   </BrowserRouter>
