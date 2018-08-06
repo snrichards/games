bc255e9bcf8f5dfbf9a872e841452b0bb2a3e6ed
Steve Richards <steve.richards@gmail.com>
2018-08-05 22:12:24 -0700

    Use semantic html for navigation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <div>
+  <Fragment>
     <h1>Games</h1>
-    <ul>
-      <li>
-        <Link to="/blackjack">Blackjack</Link>
-      </li>
-      <li>
-        <Link to="/towerofhanoi">Tower of Hanoi</Link>
-      </li>
-      <li>
-        <Link to="/tictactoe">Tic Tac Toe</Link>
-      </li>
-      <li>
-        <Link to="/sudoku">Sudoku</Link>
-      </li>
-      <li>
-        <Link to="/connectfour">Connect Four</Link>
-      </li>
-    </ul>
-  </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/blackjack">Blackjack</Link>
+        </li>
+        <li>
+          <Link to="/towerofhanoi">Tower of Hanoi</Link>
+        </li>
+        <li>
+          <Link to="/tictactoe">Tic Tac Toe</Link>
+        </li>
+        <li>
+          <Link to="/sudoku">Sudoku</Link>
+        </li>
+        <li>
+          <Link to="/connectfour">Connect Four</Link>
+        </li>
+      </ul>
+    </nav>
+  </Fragment>
 );
 
 export default Home;
